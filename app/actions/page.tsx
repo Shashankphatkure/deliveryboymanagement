d134ebad078efee973936fd2eb1795471a10f8b7
Shashank Phatkure <shashankphatkure@gmail.com>
2024-03-14 15:50:21 +0530

    delivery boy app

---
+++ app/actions/page.tsx
@@ -0,0 +1,8 @@
+import DashboardSidebar from "@/components/application-shells/sidebar";
+import React from "react";
+
+const page = () => {
+  return <DashboardSidebar />;
+};
+
+export default page;
