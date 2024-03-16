d79234f76b0e71a016ce589f48f00f90ea8286d2
Shashank Phatkure <shashankphatkure@gmail.com>
2024-03-16 20:46:56 +0530

    updated routes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,23 @@
 import DashboardSidebar from "@/components/application-shells/sidebar";
+import DriverReview from "@/components/driver/driverreview";
+import EmbeddedContent from "@/components/googlemaps/locations";
+import TableOrders from "@/components/lists/tableorders";
+import DriverPhoto from "@/components/page-headings/headings";
+import DriverOverview from "@/components/productoverview/productoverview";
 import React from "react";
 
 const page = () => {
-  return <DashboardSidebar />;
+  return (
+    <main className="py-10 lg:pl-72">
+      <div className="px-4 sm:px-6 lg:px-8">
+        <DriverPhoto />
+        <DriverOverview />
+        <TableOrders />
+        <DriverReview />
+        <EmbeddedContent />
+      </div>
+    </main>
+  );
 };
 
 export default page;
